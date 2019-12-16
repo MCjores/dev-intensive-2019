edeede2e2019e26674d28a489f2b253eeb89baed
Georg <awp-00796@mail.ru>
2019-12-16 23:57:43 +0300

    Hometask_3 поменял цвета rgb как в задании

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -35,8 +35,8 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
 enum class Status(val color: Triple<Int, Int, Int>) {
     NORMAL(Triple(255, 255, 255)),
     WARNING(Triple(255, 120, 0)),
-    DANGER(Triple(255, 60, 260)),
-    CRITICAL(Triple(255, 255, 0));
+    DANGER(Triple(255, 60, 60)),
+    CRITICAL(Triple(255, 0, 0));
 
     //поочередно будет возвращаться итемы из перечислений
     fun nextStatus(): Status {
